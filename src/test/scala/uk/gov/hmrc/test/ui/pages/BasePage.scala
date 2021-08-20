6eb6d77c0b7dc904bd92dc485c640caa5341abe4
ILee15 <89247111+ILee15@users.noreply.github.com>
2021-08-20 11:19:50 +0100

    movie name page ATs

--- src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
@@ -1,7 +1,21 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ */
+
 package uk.gov.hmrc.test.ui.pages
 
 import org.scalatest.matchers.should.Matchers
+import uk.gov.hmrc.test.ui.driver.BrowserDriver
 
-trait BasePage extends Matchers {
+trait BasePage extends Matchers with BrowserDriver {
   val url: String
+
+  def goTo(): Unit = {
+    driver.navigate.to(url)
+  }
+
+  def getTitle(): String = {
+    driver.getTitle
+  }
 }
