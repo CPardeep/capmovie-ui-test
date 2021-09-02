08c880b9b938b063feeb117f572ec35f35c9ad20
RGlossop <75027918+RGlossop@users.noreply.github.com>
2021-09-02 10:47:01 +0100

    tests for movie page

--- src/test/resources/features/HomePage.feature
+++ src/test/resources/features/HomePage.feature
@@ -4,3 +4,8 @@
     Scenario: Homepage displays list of movies
       Given I am on the homepage
       Then I can view the list of movies
+
+    Scenario: MoviePage shows a movie
+      Given I am on the homepage
+      When I click on a movie link
+      Then I am redirected to that movies page
---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/ViewOnePage.scala
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.test.ui.pages
+
+import org.openqa.selenium.{By, WebElement}
+
+object ViewOnePage extends BasePage {
+  val url: String = "http://localhost:9001/capmovie/movie/MOV0001"
+  val title: String = "The Navigator"
+
+  def movieName: WebElement = driver.findElement(By.id("title"))
+}
