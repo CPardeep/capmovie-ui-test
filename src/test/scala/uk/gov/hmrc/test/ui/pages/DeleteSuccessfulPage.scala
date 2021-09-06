ec0068238bf98e8c4552fdfb435da467e196f2d7
RGlossop <75027918+RGlossop@users.noreply.github.com>
2021-09-06 15:18:53 +0100

    tests for delete movie

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/DeleteSuccessfulPage.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ */
+
+package uk.gov.hmrc.test.ui.pages
+
+import org.openqa.selenium.{By, WebElement}
+import uk.gov.hmrc.test.ui.pages.DeleteAreYouSurePage.driver
+
+object DeleteSuccessfulPage extends BasePage {
+  val url: String = "http://localhost:9001/capmovie/delete/confirmed/MOV0001"
+  val title: String = "Delete Successful"
+
+  def confirmButton: WebElement = driver.findElement(By.id("confirm"))
+}
